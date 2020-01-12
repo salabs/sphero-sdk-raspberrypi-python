c6db35ff164fbd7b68d46cb13d7fd78d9f8284f6
Ari Keltamäki <ari.keltamaki@siili.com>
2020-01-12 22:27:13 +0200

    relocate project

---
+++ projects/sphero_RVR_RF_robocon2020/sphero_rvr.py
@@ -0,0 +1,211 @@
+import os
+import sys
+import time
+from robot.api import logger
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+
+from sphero_sdk import SpheroRvrObserver
+from sphero_sdk import RvrStreamingServices
+from sphero_sdk import Colors
+from sphero_sdk import RvrLedGroups
+
+### This import for moving RVR tests with raw motors
+from sphero_sdk import RawMotorModesEnum
+
+
+#def color_detected_handler(color_detected_data):
+#    logger.info('Color detection data response: {}'.format(color_detected_data))
+
+def color_detected_handler(color_detected_data):
+    with open("color.txt", 'a+') as fp:
+        fp.write(color_detected_data)
+
+class SetAllLeds:
+
+    def test_leds(self):
+        """ This program demonstrates how to set the all the LEDs.
+        """
+        rvr = SpheroRvrObserver()
+
+        rvr.wake()
+
+        # Give RVR time to wake up
+        time.sleep(2)
+
+        rvr.set_all_leds(
+            led_group=RvrLedGroups.all_lights.value,
+            led_brightness_values=[color for _ in range(10) for color in Colors.off.value]
+        )
+
+        # Delay to show LEDs change
+        time.sleep(1)
+
+        rvr.set_all_leds(
+            led_group=RvrLedGroups.all_lights.value,
+            led_brightness_values=[color for _ in range(10) for color in [255, 0, 0]]
+        )
+
+        # Delay to show LEDs change
+        time.sleep(1)
+        
+        rvr.close()
+
+        ###  RVR basic commands (maybe these to a setup file?)
+
+    def wake_rvr(self):
+        """ This program will wake up RVR.
+        """
+        rvr = SpheroRvrObserver()
+
+        rvr.wake()
+
+        # Give RVR time to wake up
+        time.sleep(2)
+
+    def shutdown_rvr(self):
+        """ This program will shut down RVR.
+        """
+        rvr = SpheroRvrObserver()
+
+        time.sleep(1)
+
+        rvr.close()
+
+
+        ### RVR Drive commands
+
+    def drive_forward_crawl(self):
+        """ This program will move RVR forwards slow.
+        """
+        rvr = SpheroRvrObserver()
+
+        rvr.reset_yaw()
+
+        rvr.drive_control.drive_forward_seconds(
+            speed=64,
+            heading=0,  # Valid heading values are 0-359
+            time_to_drive=.5
+        )
+        rvr.close()
+
+    def drive_forward_rally(self):
+        """ This program will move RVR forward fast.
+        """
+        rvr = SpheroRvrObserver()
+
+        rvr.reset_yaw()
+
+        rvr.drive_control.drive_forward_seconds(
+            speed=128,
+            heading=0,  # Valid heading values are 0-359
+            time_to_drive=.5
+        )
+        rvr.close()
+
+    def drive_forward_lightspeed(self):
+        """ This program will move RVR forward at lightspeed.
+        """
+        rvr = SpheroRvrObserver()
+
+        rvr.reset_yaw()
+
+        rvr.drive_control.drive_forward_seconds(
+            speed=255,
+            heading=0,  # Valid heading values are 0-359
+            time_to_drive=.2
+        )
+        rvr.close()
+
+    def drive_backward(self):
+        rvr = SpheroRvrObserver()
+
+        rvr.reset_yaw()
+
+        rvr.drive_control.drive_backward_seconds(
+            speed=16,
+            heading=0,  # Valid heading values are 0-359
+            time_to_drive=.5
+        )
+        rvr.close()
+
+    def lefty(self):
+        rvr = SpheroRvrObserver()
+
+        rvr.reset_yaw()
+
+        rvr.drive_control.turn_left_degrees(
+            heading=0,  # Valid heading values are 0-359
+            amount=90
+        )
+
+    def righty(self):
+        rvr = SpheroRvrObserver()
+
+        rvr.reset_yaw()
+
+        rvr.drive_control.turn_right_degrees(
+            heading=0,  # Valid heading values are 0-359
+            amount=90
+        )
+
+    #def do_the_dance(self):
+    #    rvr = SpheroRvrObserver()
+
+    #    rvr.reset_yaw()
+
+
+
+    #def detect_color(self, obs_time):
+    #    #data = "{}".format(color_detected_data)
+    #    #print(data)
+    #    #return data
+
+    #    """ This method uses the color sensor on RVR (located on the down side of RVR, facing the floor)
+    #        to report colors detected for obs_time seconds.
+    #    """
+    #    rvr = SpheroRvrObserver()
+    #    rvr.close()
+    #    try:
+    #        rvr.wake()
+    #
+    #       # Give RVR time to wake up
+    #       time.sleep(2)
+    #
+    #        rvr.enable_color_detection(is_enabled=True)
+    #        rvr.sensor_control.add_sensor_data_handler(
+    #            service=RvrStreamingServices.color_detection,
+    #            handler=color_detected_handler
+    #        )
+    #        rvr.sensor_control.start(interval=250)
+    #
+    #        # Allow this program to run for 2 seconds
+    #        time.sleep(0.25)
+    #
+    #    finally:
+    #        rvr.sensor_control.clear()
+    #
+    #        # Delay to allow RVR issue command before closing
+    #        time.sleep(.5)
+    #
+    #        rvr.close()
+
+    #def detect_color(self, obs_time):
+    #    rvr = SpheroRvrObserver()
+    #    rvr.close()
+    #    rvr.wake()
+    #    # Give RVR time to wake up
+    #    time.sleep(2)
+    #    rvr.enable_color_detection(is_enabled=True)
+    #    rvr.sensor_control.add_sensor_data_handler(
+    #        service=RvrStreamingServices.color_detection,
+    #        handler=color_detected_handler
+    #    )
+    #    rvr.sensor_control.start(interval=250)
+    #    # Allow this program to run for obs_time seconds
+    #    time.sleep(obs_time)
+    #    rvr.sensor_control.clear()
+    #    # Delay to allow RVR issue command before closing
+    #    time.sleep(.5)
+
+    #    rvr.close()
---
+++ projects/sphero_RVR_RF_robocon2020/sphero_rvr.robot
@@ -0,0 +1,79 @@
+*** Settings ***
+Library             OperatingSystem
+Library             Dialogs
+Library             ./sphero_rvr.py
+#Library             ./set_single_led_robot_mod.py
+Variables           ./sphero_rvr.py
+
+*** Test Cases ***
+
+Run a query for RVR actions
+    Ask Driver for directions
+
+*** Keywords ***
+
+Ask Driver for directions
+    ${selections}=  Get Selections From User   Select which actions RVR will execute:  Drive Forward Crawling  Drive Forward Rallying  Drive Backwards  Turn Right  Turn Left  Go Lightspeed (WARNING!!! need SPACE!)
+      FOR  ${option}  IN  @{selections}
+	        Run Keyword If 	'${option}' == 'Drive Forward Crawling' 	                Run keyword  Crawling
+	        Run Keyword If 	'${option}' == 'Drive Forward Rallying' 	                Run keyword  Rallying
+	        Run Keyword If 	'${option}' == 'Drive Backwards' 	                        Run keyword  Backwards
+	        Run Keyword If 	'${option}' == 'Turn Right'               	                Run keyword  Turn Right
+	        Run Keyword If 	'${option}' == 'Turn Left'               	                Run keyword  Turn Left
+	        Run Keyword If 	'${option}' == 'Go Lightspeed (WARNING!!! needs SPACE!)' 	Run keyword  Lightspeed
+	        END
+            Continue or quit
+
+Continue or quit
+    ${redrive}=  Get selection from user  Try again?  YES - New drive  NO - Quit executing tasks and exit
+	        Run Keyword If 	'${redrive}' == 'YES - New drive' 	                        Run keyword  Redrive
+	        Run Keyword If 	'${redrive}' == 'NO - Quit executing tasks and exit' 	    log to console  Bye bye!
+
+Redrive
+    Ask Driver for directions
+
+Crawling
+    Wake Rvr
+    Drive Forward Crawl
+    Shutdown Rvr
+
+Rallying
+    Wake Rvr
+    Drive Forward Rally
+    Shutdown Rvr
+
+Backwards
+    Wake Rvr
+    Drive Backward
+    Shutdown Rvr
+
+Turn Right
+    Wake Rvr
+    Righty
+    Shutdown Rvr
+
+Turn Left
+    Wake Rvr
+    Lefty
+    Shutdown Rvr
+
+Lightspeed
+    Wake Rvr
+    Drive Forward Lightspeed
+    Shutdown Rvr
+
+
+#Color detection
+#    Wake Rvr
+#    Detect Color    ${5}
+#    Sleep  7
+#    ${content}=    Get file    color.txt
+#    Log    ${content}  console=True
+#    Shutdown Rvr
+
+#Detect color
+#    Wake Rvr
+#    ${log}=  Detect Color  ${10}
+#    #${detect_color}=  color_detected_handler
+#    log to console  ${log}
+#    Shutdown Rvr
